a4c1edb90adfac07f83de5048cace9d520a4cd5b
Filipe <filipecesar2019@gmail.com>
2025-08-04 01:08:36 -0300

    upgrade

---
+++ aula11/VariaveisChar.java
@@ -0,0 +1,12 @@
+public class VariaveisChar {
+    public static void main(String[] args) {
+        char o = 'o';
+        char i = 'i';
+    System.out.println(o+i);
+    System.out.println("" + o + i);
+        char o1 = 111; // 'o' em ASCII
+        char i1 = 105; // 'i' em ASCII
+        System.out.println(o1 + i1);
+        
+}
+}
---
+++ aula11/VariaveisInteiras.java
@@ -0,0 +1,14 @@
+public class VariaveisInteiras {
+    public static void main(String[] args) {
+        byte idade01 = 20;
+        short idade02 = 21;
+        int idade03 = 22;
+        long idade04 = 23;
+        System.out.println("Idade 01: " + idade01);    
+        System.out.println("Idade 02: " + idade02);
+        System.out.println("Idade 03: " + idade03);
+        System.out.println("Idade 04: " + idade04);
+    }
+}
+    
+    
