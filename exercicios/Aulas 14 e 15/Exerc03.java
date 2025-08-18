9801f0c24ec46c2452192f349b65e5cfd7b5f89c
Filipe <filipecesar2019@gmail.com>
2025-08-18 13:53:42 -0300

    upgrade

--- exercicios/Aulas 14 e 15/Exerc03.java
+++ exercicios/Aulas 14 e 15/Exerc03.java
@@ -11,13 +11,30 @@ public class Exerc03 {
         //     System.out.println("O sexo informado eh feminino.");
         // } else {
         //     System.out.println("Sexo invalido.");
+        // } 
+
+        // Simplified version using equals method
+
+        // if ("M".equals(sexo)) {
+        //     System.out.println("O sexo informado eh masculino.");
+        // } else if ("F".equals(sexo)) {
+        //     System.out.println("O sexo informado eh feminino.");
+        // } else {
+        //     System.out.println("Sexo invalido.");
         // }
-        if ("M".equals(sexo)) {
-            System.out.println("O sexo informado eh masculino.");
-        } else if ("F".equals(sexo)) {
-            System.out.println("O sexo informado eh feminino.");
-        } else {
-            System.out.println("Sexo invalido.");
+
+        // Simplified version using switch statement
+        
+        switch (sexo) {
+            case "M":
+                System.out.println("O sexo informado eh masculino.");
+                break;
+            case "F":
+                System.out.println("O sexo informado eh feminino.");
+                break;
+            default:
+                System.out.println("Sexo invalido.");
+                break;
         }
     }
 }
