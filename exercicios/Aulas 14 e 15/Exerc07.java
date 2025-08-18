a3f72e8c0e577de415bf56abaeb1bfe37785fcca
Filipe <filipecesar2019@gmail.com>
2025-08-18 13:48:01 -0300

    update

--- exercicios/Aulas 14 e 15/Exerc07.java
+++ exercicios/Aulas 14 e 15/Exerc07.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-public class Exerc06 {
+public class Exerc07 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Entre com o primeiro número: ");
@@ -10,17 +10,25 @@ public class Exerc06 {
         int num2 = scan.nextInt();
         System.out.println("Entre com o terceiro número: ");
         int num3 = scan.nextInt();
-        
-        // Verifica o maior número
+
+         // Verifica o maior número
         int maior = num1;
         if (num2 > maior) {
             maior = num2;
         }
         if (num3 > maior) {
             maior = num3;
+        }       
+
+        // Verifica o menor número
+        int menor = num1;
+        if (num2 < menor) {
+            menor = num2;
         }
-        
+        if (num3 < menor) {
+            menor = num3;
+        }
         System.out.println("O maior número é: " + maior);
-        
+        System.out.println("O menor número é: " + menor);
     }
 }
