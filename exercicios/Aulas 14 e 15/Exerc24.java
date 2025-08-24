165db0aa3590cc681e40e1bf02b306e08ff3d115
Filipe César <filipecesarufpe@gmail.com>
2025-08-23 22:08:52 -0300

    update

---
+++ exercicios/Aulas 14 e 15/Exerc24.java
@@ -0,0 +1,60 @@
+
+import java.util.Scanner;
+
+public class Exerc24 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Entre com o primeiro número:");
+        int num1 = scanner.nextInt();
+        System.out.println("Entre com o segundo número:");
+        int num2 = scanner.nextInt();
+        System.out.println("Entre com a operação (+, -, *, /): ");
+        char operacao = scanner.next().charAt(0);
+        double resultado;
+        switch (operacao) {
+            case '+':
+                resultado = num1 + num2;
+                System.out.println("Resultado: " + resultado);
+                break;
+            case '-':
+                resultado = num1 - num2;
+                System.out.println("Resultado: " + resultado);
+                break;
+            case '*':
+                resultado = num1 * num2;
+                System.out.println("Resultado: " + resultado);
+                break;
+            case '/':                
+                if (num2 != 0) {
+                    resultado = num1 / num2;
+                    System.out.println("Resultado: " + resultado);
+                } else {
+                    System.out.println("Erro: Divisão por zero não é permitida.");
+                }
+                break;
+            default:
+                System.out.println("Operação inválida.");
+                break;
+            
+            if (resultado % 2 == 0) {
+                    System.out.println("O resultado é par.");
+                } else {
+                    System.out.println("O resultado é ímpar.");
+                }
+            if (resultado > 0) {
+                    System.out.println("O resultado é positivo.");
+                } else if (resultado < 0) {
+                    System.out.println("O resultado é negativo.");
+                } else {
+                    System.out.println("O resultado é zero.");
+                }
+            if (resultado % 1 == 0) {
+                    System.out.println("O resultado é um número inteiro.");
+                } else {
+                    System.out.println("O resultado é um número decimal.");
+                }
+                
+        
+        }
+    }
+}
