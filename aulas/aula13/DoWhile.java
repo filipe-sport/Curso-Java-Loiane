b4a87235096e21a76046cf2323527a41e406bcea
Filipe <filipecesar2019@gmail.com>
2025-08-25 23:33:05 -0300

    update

---
+++ aulas/aula13/DoWhile.java
@@ -0,0 +1,9 @@
+public class DoWhile {
+    public static void main(String[] args) {
+        int contador = 10;
+        do {
+            System.out.println("Contador: " + contador);
+            contador++;
+        } while (contador < 10);
+    }
+}
