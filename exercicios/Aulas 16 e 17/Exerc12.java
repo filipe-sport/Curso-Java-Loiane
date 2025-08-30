9af15a9272369f1c50f07ad728cdd80e5b75432c
Filipe <filipecesar2019@gmail.com>
2025-08-30 20:40:56 -0300

    update

---
+++ exercicios/Aulas 16 e 17/Exerc12.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class Exerc12 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner (System.in);
+        System.out.println("DDigite qual tabuada você quer: ");
+        int num = scan.nextInt();
+        System.out.println("Tabuada de " + num + ":");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num + " x " + i + " = " + (num * i));
+        }
+    }
+}
