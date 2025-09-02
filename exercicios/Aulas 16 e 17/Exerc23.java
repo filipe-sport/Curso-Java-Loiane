460a74ce6549630a3b6b2d967fdcddb7f91a96f5
Filipe <filipecesar2019@gmail.com>
2025-09-02 20:38:28 -0300

    update

--- exercicios/Aulas 16 e 17/Exerc23.java
+++ exercicios/Aulas 16 e 17/Exerc23.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
 
-public class Exerc22 {
+public class Exerc23 {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+         Scanner scan = new Scanner(System.in);
         System.out.println("Entre com um número inteiro: ");
         int num = scan.nextInt();
         int i = 2;
+        int cont = 0;
         if (num < 2 ){
             System.out.println("O número " + num + "não é primo!");
         }else{
@@ -17,13 +18,15 @@ public class Exerc22 {
                     while (i < num){
                    if (num % i == 0){
                        System.out.println("O numero "+ num + " é divisível por "+i);
+                       cont++; 
                    }
-                   i++;               
+                   i++; 
+                                
                    }
                 }
                 i++;
             }
         }
+        System.out.println("O número de divisões efetuadas foi: " + cont);
     }
-                
-}    
+}
