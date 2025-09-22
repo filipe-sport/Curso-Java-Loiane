4779f7dec740758ba29cef1048da66f494d73e60
Filipe <filipecesar2019@gmail.com>
2025-09-22 20:35:16 -0300

    update

--- aulas/com.aula24/Carro.java
+++ aulas/com.aula24/Carro.java
@@ -4,4 +4,8 @@ public class Carro {
     int numPassageiros;
     double capCombustivel;
     double consumoCombustivel;
+    public void exibirAutonomia() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'exibirAutonomia'");
+    }
 }
